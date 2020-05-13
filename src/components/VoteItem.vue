57946f94705d113c345eb206ea8eee3485046a4e
sondreal <sondre@villdyr.no>
2020-05-13 23:34:17 +0200

    So many changes ⚡

---
+++ src/components/VoteItem.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div class="section">
+      <article class="media">
+        <figure class="media-left">
+          <img
+            :src="require(`@/assets/images/submissions/${upVotesImage}.png`)"
+            alt=""
+            class="image is-64x64"
+          />
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>
+                <a href="">{{ upVotesTitle }}</a>
+                <span class="tag is-small">#{{ upVotesNumberTag }}</span>
+              </strong>
+              <br />
+              {{ upVotesText }}
+              <br />
+              <small class="is-size-7">
+                Submitted by {{ upVotesSumbitter }}
+                <img
+                  :src="require(`@/assets/images/avatars/${upVotesAvatar}.jpg`)"
+                  class="image is-24x24"
+                  alt=""
+                />
+              </small>
+            </p>
+          </div>
+        </div>
+        <div class="media-right">
+          <span class="icon is-small" v-on:click="upvote(upVotesNumberTag)">
+            <i class="fa fa-chevron-up"></i>
+            <strong class="has-text-info">{{ upVotes }}</strong>
+          </span>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "upVotes",
+    "upVotesText",
+    "upVotesNumberTag",
+    "upVotesTitle",
+    "upVotesSumbitter",
+    "upVotesImage",
+    "upVotesAvatar"
+  ],
+  methods: {
+    upvote(submissionId) {
+      this.$emit("update-votes", submissionId);
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.section {
+  padding: 2rem 1.5rem;
+}
+
+.media {
+  max-width: 600px;
+  margin: 0 auto;
+  border: 1px solid #e6e7e9;
+  padding: 1em 1.5em 0.5em 1.5em;
+  border-radius: 0.3em;
+}
+
+.media + .media {
+  margin-top: 1.5rem;
+}
+
+.blue-border {
+  border: 1px solid #3373dc !important;
+}
+
+.tag {
+  font-size: 0.6rem !important;
+}
+
+.icon {
+  cursor: pointer;
+}
+
+.image.is-24x24 {
+  display: inline;
+  position: relative;
+  top: 5px;
+  border-radius: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2 class="title has-text-centered dividing-header">UpVotey!</h2>
+    <VoteItem
+      v-for="submission in sortedSubmissions"
+      :key="submission.id"
+      :upVotesText="submission.description"
+      :upVotesNumberTag="submission.id"
+      :upVotesTitle="submission.title"
+      :upVotesSumbitter="submission.submitter"
+      :upVotes="submission.votes"
+      :upVotesImage="submission.submissionImage"
+      :upVotesAvatar="submission.avatar"
+      @update-votes="updateVotes"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import VoteItem from "@/components/VoteItem.vue";
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld
+    VoteItem
+  },
+  data() {
+    return {
+      submissions: [
+        {
+          id: 1,
+          title: "Code Vue",
+          description: "I will code VueJS all day.",
+          url: "#",
+          votes: 16,
+          avatar: "daniel",
+          submissionImage: "image-aqua",
+          submitter: "Sondre"
+        },
+        {
+          id: 2,
+          title: "Laravel all day",
+          description: "Laravel all day! ! !.",
+          url: "#",
+          votes: 20,
+          avatar: "daniel",
+          submissionImage: "image-rose",
+          submitter: "Sondre"
+        },
+        {
+          id: 3,
+          title: "DevOps",
+          description: "We will create some dank server networks on stream.",
+          url: "#",
+          votes: 14,
+          avatar: "daniel",
+          submissionImage: "image-yellow",
+          submitter: "Sondre"
+        },
+        {
+          id: 4,
+          title: "GoLang",
+          description: "We will make some really dank software with GoLang.",
+          url: "#",
+          votes: 7,
+          avatar: "daniel",
+          submissionImage: "image-steel",
+          submitter: "Sondre"
+        }
+        // ...
+        // ...
+      ]
+    };
+  },
+  computed: {
+    sortedSubmissions () {
+      return this.submissions.slice().sort((a, b) => {
+        return b.votes - a.votes;
+      });
+    }
+  },
+  methods: {
+    updateVotes(e) {
+      const submission = this.submissions.find(
+        submission => submission.id === e
+      );
+      submission.votes++;
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.dividing-header {
+  margin-top: 1em;
+  margin-bottom: 2em;
+}
+</style>
